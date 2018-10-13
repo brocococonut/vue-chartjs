1e5e6e4f2cdbb0df6da092e12f4f5a3568be2c40
Jake Lees <brocococonut@gmail.com>
2018-10-14 09:32:05 +1100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,28 +1,28 @@
 import mixins from './mixins/index.js'
 
 import {
-  Bar,
-  HorizontalBar,
-  Doughnut,
+//   Bar,
+//   HorizontalBar,
+//   Doughnut,
   Line,
-  Pie,
-  PolarArea,
-  Radar,
-  Bubble,
-  Scatter,
+//   Pie,
+//   PolarArea,
+//   Radar,
+//   Bubble,
+//   Scatter,
   generateChart
 } from './BaseCharts'
 
 const VueCharts = {
-  Bar,
-  HorizontalBar,
-  Doughnut,
+//   Bar,
+//   HorizontalBar,
+//   Doughnut,
   Line,
-  Pie,
-  PolarArea,
-  Radar,
-  Bubble,
-  Scatter,
+//   Pie,
+//   PolarArea,
+//   Radar,
+//   Bubble,
+//   Scatter,
   mixins,
   generateChart,
   render: () => console.error('[vue-chartjs]: This is not a vue component. It is the whole object containing all vue components. Please import the named export or access the components over the dot notation. For more info visit https://vue-chartjs.org/#/home?id=quick-start')
@@ -32,15 +32,15 @@ export default VueCharts
 
 export {
   VueCharts,
-  Bar,
-  HorizontalBar,
-  Doughnut,
+//   Bar,
+//   HorizontalBar,
+//   Doughnut,
   Line,
-  Pie,
-  PolarArea,
-  Radar,
-  Bubble,
-  Scatter,
+//   Pie,
+//   PolarArea,
+//   Radar,
+//   Bubble,
+//   Scatter,
   mixins,
   generateChart
 }
